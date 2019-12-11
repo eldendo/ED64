51d154b19d25d65d805c542d373d015699213ffe
eldendo <marc.dendooven@gmail.com>
2019-12-11 20:27:41 +0100

    Add files via upload

---
+++ ed64Chapter11/SystemAbstraction.pas
@@ -0,0 +1,171 @@
+(*****************************************************************
+* SystemAbstraction unit                                         *
+* This file is part of ED64                                      *
+* Copyright (c) 2006 - 2009 by ir. Marc Dendooven                *
+* Version: Chapter 10 (same as chapter 9)                        *
+*****************************************************************)
+Unit SystemAbstraction;
+
+
+
+(****************************************************************)
+interface
+
+
+
+type nibble = 0..15;
+
+procedure openScreen;
+procedure closeScreen;
+procedure setPixel(x,y : word; color: nibble);
+function closeScreenRequest : Boolean;
+
+procedure aWriteLn (s: string);
+function aKeyPressed : boolean;
+function aReadKey : char;
+
+function getFileName(msg:string): string;
+
+(****************************************************************)
+implementation
+
+
+uses ptcGraph, ptcCRT;// memio;
+
+
+var gd,gm : integer;
+    PixelSize : byte;
+    closeRequest: boolean = false;
+(*
+procedure OpenScreen;
+Begin
+ Writeln('Graphics Initialised.');
+ gd := Detect;
+ InitGraph(gd, gm,'');
+end;
+*)
+procedure openscreen;
+var res : char;
+begin
+    writeln ('Choose graphic resolution');
+    writeln;
+    writeln ('0. Default       4. 1024x768');
+    writeln ('1. 320x200       5. 1280x1024');
+    writeln ('2. 640x480                   ');
+    writeln ('3. 800x600                   ');
+    writeln;
+    writeln ('Your choise : (another value sets to Default) ');
+    readln (res);
+    case res of
+        '1' : gm := m320x200;
+        '2' : gm := m640x480;
+        '3' : gm := m800x600;
+        '4' : gm := m1024x768;
+        '5' : gm := m1280x1024;
+        else
+            gm := detectMode
+    end;
+
+    gd := D8bit;
+
+    initgraph(gd,gm,'ED64');
+	if graphresult <> grOK then
+	begin
+			writeln('graphic mode not supported');
+			readln;
+			halt(1);
+    end;
+    pixelsize := (getMaxX+1) div 320;
+
+    SetBkColor(14);
+    ClearDevice;
+    setviewport((getmaxX-320*pixelSize) div 2,(getMaxY-200*pixelSize) div 2,getmaxX,getMaxY,clipoff);
+
+(*    setPalette(0,black);
+    setPalette(1,white);
+    setPalette(2,red);
+    setPalette(3,cyan);
+    setPalette(4,magenta);
+    setPalette(5,green);
+    setPalette(6,blue);
+    setPalette(7,yellow);
+    setPalette(8,lightred);
+    setPalette(9,brown);
+    setPalette(10,lightred);
+    setPalette(11,darkgray);
+    setPalette(12,lightgray);
+    setPalette(13,lightgreen);
+    setPalette(14,lightblue);
+    setPalette(15,lightgray) *)
+    
+ (* followin colorscheme :   
+    http://www.pepto.de/projects/colorvic/
+    Philip "Pepto" Timmermann, <pepto@pepto.de>
+ *)   
+setRGBpalette(0,$00, $00, $00);
+setRGBpalette(1,$FF, $FF, $FF);
+setRGBpalette(2,$68, $37, $2B);
+setRGBpalette(3,$70, $A4, $B2);
+setRGBpalette(4,$6F, $3D, $86);
+setRGBpalette(5,$58, $8D, $43);
+setRGBpalette(6,$35, $28, $79);
+setRGBpalette(7,$B8, $C7, $6F);
+setRGBpalette(8,$6F, $4F, $25);
+setRGBpalette(9,$43, $39, $00);
+setRGBpalette(10,$9A, $67, $59);
+setRGBpalette(11,$44, $44, $44);
+setRGBpalette(12,$6C, $6C, $6C);
+setRGBpalette(13,$9A, $D2, $84);
+setRGBpalette(14,$6C, $5E, $B5);
+setRGBpalette(15,$95, $95, $95);
+
+end;
+
+procedure closeScreen;
+begin
+    closeGraph
+end;
+
+procedure setPixel(x,y : word; color : nibble);
+begin
+//    putpixel(x,y,color)
+	setFillStyle(solidFill,color);
+	Bar(x*pixelSize,y*pixelSize,(x+1)*pixelSize-1,(y+1)*pixelSize-1);
+end;
+
+function closeScreenRequest : Boolean;
+begin
+    closeScreenRequest := closeRequest
+end;
+
+procedure aWriteLn (s: string);
+begin
+    outText(s)
+end;
+
+function aKeyPressed : boolean;
+begin
+    aKeyPressed := keyPressed;
+    delay(5)
+end;
+
+function aReadKey : char;
+begin
+    aReadKey := readKey;
+    if aReadKey = #3 then closeRequest := true;
+end;
+
+function getFileName(msg:string): string;
+var filename : string ='';
+	ch: char;
+begin
+    outText(msg);
+    repeat
+		if keypressed then begin 	ch := readKey;
+									if ch <> chr(13) then begin outText(ch); filename := filename+ch end 
+						   end
+    until ch = chr(13);
+    getFileName := filename;
+end;
+
+end.
---
+++ ed64Chapter11/vic.pas
@@ -0,0 +1,201 @@
+(*****************************************************************
+* ED64 : EL DENDO's Commodore 64 emulator                        *
+* VIC emulation unit                                             *
+* (c) 2006 - 2019 by ir. Marc Dendooven                          *
+* Versie Chapter 11                                              *
+*****************************************************************)
+
+Unit vic;
+(****************************************************************)
+interface
+
+uses SystemAbstraction;
+
+procedure vicVMupdate(pos: word; val: byte);
+procedure vicPMupdate(pos: word; val: byte);
+function  vicRead(address : word) : byte;
+procedure vicWrite(address : word ; val : byte);
+procedure colorWrite(address : word; color : nibble);
+function  colorRead(address : word) : nibble;
+procedure vicBank(val : byte);
+(****************************************************************)
+
+implementation
+
+uses memio;
+
+const CMbase = $D800;
+       SCM = $00;
+       MCCM = $10;
+       ECCM = $40;
+       SBMM = $20;
+       MCBMM = $30;
+
+var
+    bankAddress : word;
+    VMbase : word;
+    PMbase : word;
+    reg : array[$D000..$D02E] of byte;
+    color_ram : array[$D800..$DBFF] of nibble;
+
+    bmm : boolean;
+    vidMode : byte = $00;
+    PMlenght : word = 1999;
+
+procedure refresh;
+var i : word;
+begin
+        for i := 0 to 999 do poke (VMbase+i,peek(VMbase+i))
+end;
+
+procedure setVicValues;
+begin
+        vidMode := (reg[$D011] and $60) or (reg[$D016] and $10);
+(*      case vidMode of
+            SCM    : writeln ('video mode : SCM');
+            MCCM   : writeln ('video mode : MCCM');
+            ECCM   : writeln ('video mode : ECCM');
+            SBMM   : writeln ('video mode : SBMM');
+            MCBMM  : writeln ('video mode : MCBMM')
+        end;   *)
+        bmm := boolean (vidmode and $20);
+        VMbase := bankAddress + (reg[$D018] shr 4)*$400;
+        if bmm then begin
+                        PMbase := bankAddress + (reg[$D018] and $08)*$400;
+                        PMlenght := 7999
+                     end
+               else begin
+                        PMbase := bankAddress + (reg[$D018] and $0E)*$400;
+                        PMlenght := 1999
+                     end;
+        setVicVMwatchRange(VMbase,VMbase+999);
+        setVicPMwatchRange(PMbase,PMbase+PMlenght);
+//       refresh
+end;
+
+procedure vicBank(val : byte);
+begin
+    bankAddress := not(val and 3) * $4000;
+    setVicValues
+end;
+
+procedure colorWrite(address : word; color : nibble);
+begin
+    color_ram[address] := color;
+    vicVMupdate(address-CMbase,peek(address-CMbase+VMbase))
+end;
+
+function colorRead(address : word) : nibble;
+begin
+    colorRead := color_ram[address]
+end;
+
+function  vicRead(address : word) : byte;
+begin
+    case address of
+        $D012 : vicRead := $00; //raster
+    else
+        vicRead := reg[address]
+    end
+end;
+
+procedure vicWrite(address : word ; val : byte);
+begin
+    reg[address] := val;
+    setVicValues
+end;
+
+procedure updateSCM(pos: word; val: byte);
+var x0,y0,x,y:integer;
+begin
+       x0:=(pos mod 40)*8;
+       y0:=(pos div 40)*8;
+       for x:=0 to 7 do
+          for y := 0 to 7 do
+             if boolean(vicPeek(PMbase+val*8+y) and ($80 shr x))
+                   then setPixel(x0+x,y0+y,color_ram[CMbase+pos])
+                   else setPixel(x0+x,y0+y,reg[$D021])
+end;
+
+procedure updateECCM(pos: word; val: byte);
+var x0,y0,x,y:integer;
+    bg : byte;
+begin
+       x0:=(pos mod 40)*8;
+       y0:=(pos div 40)*8;
+       bg := (val and $C0) shr 6;
+       val := val and $3F;
+       for x:=0 to 7 do
+          for y := 0 to 7 do
+             if boolean(vicPeek(PMbase+val*8+y) and ($80 shr x))
+                   then setPixel(x0+x,y0+y,color_ram[CMbase+pos])
+                   else setPixel(x0+x,y0+y,reg[$D021+bg])
+end;
+
+procedure updateMCCM(pos: word; val: byte);
+var x0,y0,x,y:integer;
+begin
+    if boolean(color_ram[CMbase+pos] and $8)
+        then
+            begin
+                x0:=(pos mod 40)*8;
+                y0:=(pos div 40)*8;
+                for x:=0 to 7 do
+                    for y := 0 to 7 do
+                        case (vicPeek(PMbase+val*8+y) shr (2*((7-x) div 2))) and 3 of
+                            0 : setPixel(x0+x,y0+y,reg[$D021]);
+                            1 : setPixel(x0+x,y0+y,reg[$D022]);
+                            2 : setPixel(x0+x,y0+y,reg[$D023]);
+                            3 : setPixel(x0+x,y0+y,color_ram[CMbase+pos] and $7)
+                        end
+            end
+        else updateSCM (pos,val)
+end;
+
+procedure updateSBMM(pos: word; val: byte);
+var x0,y0,x,y:integer;
+begin
+       x0:=(pos mod 40)*8;
+       y0:=(pos div 40)*8;
+       for x:=0 to 7 do
+          for y := 0 to 7 do
+             if boolean(vicPeek(PMbase+pos*8+y) and ($80 shr x))
+                   then setPixel(x0+x,y0+y,(val and $F0) shr 4)
+                   else setPixel(x0+x,y0+y,val and $0F)
+end;
+
+procedure updateMCBMM(pos: word; val: byte);
+var x0,y0,x,y:integer;
+begin
+        x0:=(pos mod 40)*8;
+        y0:=(pos div 40)*8;
+        for x:=0 to 7 do
+           for y := 0 to 7 do
+              case (vicPeek(PMbase+pos*8+y) shr (2*((7-x) div 2))) and 3 of
+                            0 : setPixel(x0+x,y0+y,reg[$D021]);
+                            1 : setPixel(x0+x,y0+y,(val and $F0) shr 4);
+                            2 : setPixel(x0+x,y0+y,val and $0F);
+                            3 : setPixel(x0+x,y0+y,color_ram[CMbase+pos])
+              end
+end;
+
+procedure vicVMupdate(pos: word; val: byte);
+begin
+    case vidMode of
+        SCM  : updateSCM (pos,val);
+        MCCM : updateMCCM(pos,val);
+        ECCM : updateECCM(pos,val);
+        SBMM : updateSBMM(pos,val);
+        MCBMM: updateMCBMM(pos,val);
+    else
+            aWriteLn ('WARNING: Undefined Video Mode !')
+    end
+end;
+
+procedure vicPMupdate(pos: word; val: byte);
+begin
+    if bmm 	then vicVMupdate(pos div 8, vicPeek(VMbase+(pos div 8)))
+        //       else refresh;
+end;
+
+end.
